ac378ec035d3368c22352e4daab1b34704ae92cf
Ari Viitala <ari.m.viitala@aalto.fi>
2019-10-29 13:58:51 +0200

    SAC works with reduced vision

---
+++ game2.py
@@ -0,0 +1,618 @@
+import pygame
+import pygame.gfxdraw
+import numpy as np
+import pandas as pd
+
+from scipy.spatial import ConvexHull
+from scipy.ndimage import rotate
+
+import cv2
+
+import matplotlib.pyplot as plt
+
+class box:
+
+    """Implements the car of the game. Accelerating, turning are done by this classes methods.
+    The car is moved every frame according to the speed and direction of the car.
+    """
+
+    def __init__(self, x, y, h, w, color, vh, vw):
+
+        self.init_x = x 
+        self.init_y = y
+        
+        self.x = x
+        self.y = y
+        self.h = h
+        self.w = w
+        self.vx = 0
+        self.vy = 0
+        self.ax = 0
+        self.ay = 0
+
+        self.v = 0
+        self.a = 0
+
+        self.dir = 0
+
+        self.color = color
+
+        self.vh = vh
+        self.vw = vw
+
+        self.turning_limit = 0.1
+
+        self.corners = np.array([[-w/2, -h/2], [w/2, -h/2], [w/2, h/2], [-w/2, h/2]])
+        self.vision_limits = np.array([[-vw/2, -vh/2], [vw/2, -vh/2], [vw/2, vh/2], [-vw/2, vh/2]])
+        
+        vc = self.vision_limits
+
+        self.vision_corners = [[vc[i,0] + self.x, vc[i,1] + self.y] for i in range(4)]
+
+    def draw(self, surface):
+        
+        draw_dir = -self.dir
+
+        c = self.corners @ -np.array([[np.cos(draw_dir), -np.sin(draw_dir)], [np.sin(draw_dir), np.cos(draw_dir)]])
+        
+        corners = [[c[i,0] + self.x, c[i,1] + self.y] for i in range(4)]
+         
+        pygame.gfxdraw.filled_polygon(surface, corners, (255, 255, 0))
+        pygame.draw.lines(surface, (0,0,0), True, corners, 3)
+        #pygame.draw.lines(surface, (0,0,0), True, self.vision_corners , 3)
+        
+
+        
+    def move(self):
+        self.v += self.a
+
+        self.x += np.sin(self.dir) * self.v 
+        self.y -= np.cos(self.dir) * self.v 
+
+        self.v *= 0.8
+
+        vc = self.vision_limits
+
+        self.vision_corners = [[vc[i,0] + self.x, vc[i,1] + self.y] for i in range(4)]
+        
+    def turn(self, direction):
+
+        """
+        This method is called to turn the car when action is given to the car.
+        """
+
+        if abs(direction) > self.turning_limit:
+            self.dir += np.sign(direction) * self.turning_limit
+        else:
+            self.dir += direction
+    
+    def accelerate(self, amount):
+
+        """
+        This method is called to change the acceleration of the car when action is given.
+        """
+
+        self.a = amount
+
+    def reset(self):
+
+        """
+        Reset the car to initial parameters.
+        """
+
+        self.x = self.init_x
+        self.y = self.init_y
+        self.v = 0
+        self.dir = 0
+        self.a = 0
+
+        vc = self.vision_limits
+
+        self.vision_corners = [[vc[i,0] + self.x, vc[i,1] + self.y] for i in range(4)]
+
+
+def get_track(w, h):
+
+    """
+    Loads the track points from a text file.
+    """
+
+    track_points = pd.read_csv("rata.csv", header = None)
+
+    outer_track = track_points.iloc[:,0:2]
+    inner_track = track_points.iloc[:24,2:4]
+
+    outer_track_points = []
+    inner_track_points = []
+
+
+    for i in range(len(outer_track)):
+        outer_track_points.append((outer_track.iloc[i, 0] / 1600 * w, outer_track.iloc[i, 1] / 1000 * h))
+
+    for i in range(len(inner_track)):
+        inner_track_points.append((inner_track.iloc[i, 0] / 1600 * w, inner_track.iloc[i, 1] / 1000 * h))
+
+    return outer_track_points, inner_track_points
+
+def draw_track(surface, inner, outer):
+
+    pygame.gfxdraw.filled_polygon(surface, outer, (60, 60, 60))
+    pygame.gfxdraw.filled_polygon(surface, inner, (13, 156, 0))
+
+    pygame.draw.lines(surface, (255, 255, 255), True, outer, 5)
+    pygame.draw.lines(surface, (255, 255, 255), True, inner, 5)
+
+def intersection(l1, l2):
+    
+    """
+    Returns the intersection of two lines l1 and l2 defined as starting and end points of those lines.
+    """
+
+    k1 = (l1[0][1] - l1[1][1]) / (l1[0][0] - l1[1][0])
+    k2 = (l2[0][1] - l2[1][1]) / (l2[0][0] - l2[1][0])
+    
+    b1 = l1[0][1] - k1 * l1[0][0]
+    b2 = l2[0][1] - k2 * l2[0][0]
+    
+    int_x = (b2 - b1) / (k1 - k2)
+    int_y = k1 * int_x + b1
+
+    x1_ok = min(l1[0][0], l1[1][0]) < int_x and max(l1[1][0], l1[0][0]) > int_x
+    y1_ok = min(l1[0][1], l1[1][1]) < int_y and max(l1[0][1], l1[1][1]) > int_y
+
+    x2_ok = min(l2[0][0], l2[1][0]) < int_x and max(l2[1][0], l2[0][0]) > int_x
+    y2_ok = min(l2[0][1], l2[1][1]) < int_y and max(l2[0][1], l2[1][1]) > int_y
+
+    #return (int_x, int_y)
+
+    if y1_ok and x1_ok and x2_ok and y2_ok:
+        return (int_x, int_y)
+    else:
+        return (-1,-1) 
+
+
+def dist(l1, i):
+
+    return (l1[0][0] - i[0])**2 + (l1[0][1] - i[1])**2
+
+
+def min_track_intersection(l1, track):
+    
+    ints = []
+    
+    for i in range(len(track)):
+        if (i != len(track) - 1):
+            l2 = (track[i], track[i + 1])
+        else:
+            l2 = (track[i], track[0])
+
+        intr = intersection(l1, l2)
+
+        if intr[0] != -1:
+            ints.append(intr)
+    
+    #print(ints)
+
+    min_d = 10000000
+    min_point = (0,0)
+
+    for i in ints:
+        d = dist(l1, i)
+        
+        if d < min_d:
+            min_d = d
+            min_point = i
+
+    return min_point
+
+def min_intersection(l1, inner, outer):
+
+    inner_int = min_track_intersection(l1, inner)
+    outer_int = min_track_intersection(l1, outer)
+
+    if inner_int[0] == 0:
+        return outer_int
+    elif dist(l1, inner_int) < dist(l1, outer_int):
+        return inner_int
+    else:
+        return outer_int
+
+
+
+def is_out(x, y, points):
+
+    """Determines (x,y) is outside the track."""
+
+    counter = 0
+    for i in range(len(points)):
+        if (i != len(points) - 1): 
+            start = points[i]
+            end = points[i + 1]
+        else:
+            start = points[i]
+            end = points[0]
+
+        if (start[0] < x and end[0] < x):
+            counter = counter
+        else:
+
+            if (end[0] == start[0]):
+                k = 1000000000
+            elif(end[1] == start[1]):
+                k = 0.000000001
+            else:
+                k = (end[1] - start[1]) * 1.0 / (end[0] - start[0])
+            
+            b = end[1] - k * end[0]
+
+            xa = (b - y) / (-k)
+
+            if((xa < max(x, min(start[0], end[0]))) or (xa > max(end[0], start[0]))):
+                counter = counter
+            else:
+                counter += 1
+
+    if counter % 2 == 0:
+        return True
+    else:
+        return False
+
+
+def control(box, style):
+
+    global running 
+
+    acc = 0.2
+    turning = 0.2
+
+    if style == "manual":
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    box.accelerate(acc)
+                if event.key == pygame.K_DOWN:
+                    box.accelerate(-acc)
+                if event.key == pygame.K_RIGHT:
+                    box.turn(turning)
+                if event.key == pygame.K_LEFT:
+                    box.turn(-turning)
+
+
+def distance(x3, y3, track):
+    distances = []
+
+    for i in range(len(track)):
+        if i == len(track) - 1:
+            start = track[-1]
+            end = track[0]
+        else:
+            start = track[i]
+            end = track[i + 1]
+
+        x1 = start[0]
+        y1 = start[1]
+        x2 = end[0]
+        y2 = end[1]
+
+        px = x2-x1
+        py = y2-y1
+
+        norm = px*px + py*py
+
+        u =  ((x3 - x1) * px + (y3 - y1) * py) / float(norm)
+
+        if u > 1:
+            u = 1
+        elif u < 0:
+            u = 0
+
+        x = x1 + u * px
+        y = y1 + u * py
+
+        dx = x - x3
+        dy = y - y3
+
+        dist = (dx*dx + dy*dy)**.5
+
+        distances.append(dist)
+
+    return np.min(distances)
+
+def process_image(obs):
+
+        result = cv2.resize(obs, (80, 80))
+
+        return result
+
+class action_space:
+
+    """Made this to implement the sample and shape functions, 
+    since I didn't know what data structure had those functions."""
+    
+    def __init__(self):
+        self.shape = (2, 1)
+    
+    def sample(self):
+        return np.random.rand(self.shape[0]) - 0.5
+
+class game:
+
+    def __init__(self, draw = True, manual_control = False):
+
+        pygame.init()
+
+        self.display_width = 1600
+        self.display_height = 1000
+        self.surface = pygame.display.set_mode((self.display_width, self.display_height))
+
+        car_x = 230 / 1600 * self.display_width
+        car_y = 400 / 1000 * self.display_height
+        # 
+
+        # car_x = 1420 / 1600 * self.display_width
+        # car_y = 800 / 1000 * self.display_height
+
+        self.car = box(car_x, car_y, 40, 20, (0,0,0), 600, 300)
+
+        self.clock = pygame.time.Clock()
+
+        self.running = True
+
+        self.outer, self.inner = get_track(self.display_width, self.display_height)
+
+        self.graphics = draw
+
+        self.manual_control = manual_control
+
+        self.line_points = []
+
+        self.n_directions = 12
+
+        self.observation_space = np.zeros(self.n_directions)
+        self.action_space = action_space()
+        self._max_episode_steps = 1000
+
+        dirs = np.linspace(-np.pi/2, np.pi/2, 8) + self.car.dir
+        
+        for d in dirs:
+            self.line_points.append((self.car.x + np.cos(d) * 10, self.car.y + np.sin(d) * 10))
+
+    def get_reduced_vision(self, n_directions=13):
+
+
+        """"
+        This implements the vision that is just the distances to the wall in 12 directions.
+        """
+
+        dirs = np.linspace(0, 2 * np.pi, n_directions)[:-1] - self.car.dir
+
+        x = self.car.x
+        y = self.car.y
+
+        line_points = []
+        dists = []
+
+        length = 100000
+
+        for d in dirs:
+            line_points.append((x + np.sin(d) * length, y + np.cos(d) * length))
+
+        for l in line_points:
+            intr = min_intersection(((self.car.x, self.car.y), l), self.outer, self.inner)
+            dist = np.sqrt((self.car.x - intr[0])**2 + (self.car.y - intr[1])**2)
+            dists.append(dist)
+
+        self.line_points = line_points
+
+        return dists
+
+
+    def get_vision(self):
+
+        pa = np.array(pygame.PixelArray(self.surface))
+
+        l = self.car.vision_corners
+
+        result = np.zeros((self.car.vw, self.car.vh))
+
+        left = max(0, int(l[0][0]))
+        right = min(self.surface.get_width(), int(l[1][0]))
+        top = max(0, int(l[0][1]))
+        bottom = min(self.surface.get_height(), int(l[3][1]))   
+
+        pad_left = 0
+        pad_right = 0
+        pad_top = 0
+        pad_bottom = 0
+
+        if left > l[0][0]:
+            pad_left = int(abs(l[0][0]))
+        elif right < l[1][0]:
+            pad_right = int(l[1][0] - right)
+
+        if top > l[0][1]:
+            pad_top = int(abs(l[0][1]))
+        elif bottom < l[3][1]:
+            pad_bottom = int(l[3][1] - bottom)
+
+        # print(pad_left)
+        # print(pad_right)
+        # print(pad_top) 
+        # print(pad_bottom)
+
+        vision = pa[left:right, top:bottom].T
+        vision = np.pad(vision, ((pad_top, pad_bottom), (pad_left, pad_right)), mode="edge")
+        
+        vision = rotate(vision, self.car.dir / np.pi * 180, reshape = False, mode = "nearest")
+
+        red = vision >> 16
+        green = (vision >> 8) - (red << 8)
+        blue = vision - (red << 16) - (green << 8)
+
+        im = np.stack((red, green, blue), axis = 2)
+        im = np.dot(im, [0.299, 0.587, 0.114])
+
+        height = im.shape[0]
+
+        res = im[:height//2, :]
+        
+        return process_image(res)
+        #return im
+    
+    def reset(self):
+        self.car.reset()
+
+        return self.get_reduced_vision()
+
+    def step(self, action, given_obs=np.zeros((80,80))):
+        
+        start_reward = self.reward()
+
+        for i in range(1):
+
+            #Here I've bound throttle to one to see if even that works
+            
+            obs, reward, done = self.frame(action[0] / 10, max(0, action[1]) * 3, given_obs) 
+            if not done:
+                continue
+            else:
+                break
+        
+        #print(reward)
+
+        return obs, reward, done, 0
+
+    def reward(self):
+
+        outer = distance(self.car.x, self.car.y, self.outer)
+        inner = distance(self.car.x, self.car.y, self.inner)
+        
+        # if self.car.v < 0:
+        #     reward = self.car.v
+        # else:
+        reward = self.car.v
+        
+        #reward = self.car.v
+
+        #print("Distance from outer: {}, Distance from inner: {}, Reward: {}".format(outer, inner, reward))
+        
+        return reward
+
+    def frame(self, steering, acc, obs):
+
+        # if self.manual_control:
+        #     control(self.car, "manual")
+        # else:
+        self.car.accelerate(acc)
+        self.car.turn(steering)
+        self.car.move()
+
+        done = False
+        rwd = self.reward()
+
+
+        if is_out(self.car.x, self.car.y, self.outer) or not is_out(self.car.x, self.car.y, self.inner):
+            self.car.reset()
+            rwd = 0
+            done = True
+
+        if self.graphics:
+            self.draw(obs)
+        
+        
+        obs = np.array(self.get_reduced_vision())
+        
+        self.clock.tick(30)
+
+        return obs, rwd, done, 
+
+
+    def draw(self, obs):
+        self.surface.fill((13, 156, 0))
+        draw_track(self.surface, self.inner, self.outer)
+        self.car.draw(self.surface)
+
+        self.get_reduced_vision()
+
+        for l in self.line_points:
+            l1 = ((self.car.x, self.car.y), l)
+            
+            i = min_intersection(l1, self.outer, self.inner)
+            pygame.draw.line(self.surface, (0,0,0), (self.car.x, self.car.y), i, 3)
+            
+            #print(i)
+            pygame.draw.rect(self.surface, (0,0,0), pygame.Rect(i[0], i[1], 10, 10))
+
+        surf = pygame.surfarray.make_surface(obs)
+        self.surface.blit(surf, (1500,0))
+
+        font = pygame.font.Font('freesansbold.ttf', 12) 
+        text = font.render("Speed: {}, Acc: {}, Direction: {}, Reward: {}".format(round(self.car.v, 2), round(self.car.a, 2), round(self.car.dir, 2), round(self.reward(), 2)), True, (0,0,0))
+        self.surface.blit(text, (10, 10))
+
+        pygame.display.update()
+
+
+
+
+
+def main():
+    
+    pygame.init()
+    
+    new_game = game()
+    
+    running = True
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            
+        new_game.step(np.random.rand() / 5 - 0.1, np.random.rand() / 5 - 0.1)
+
+if __name__ == "__main__":
+    
+    main()
+
+    # from PIL import Image
+
+    # g = game()
+
+    # acc = 0.2
+    # direction = 0.2
+
+    # running = True
+
+    # obs = np.random.rand(80, 80)
+
+    # counter = 0
+
+    # while running:
+
+    #     counter += 1
+
+    #     cur_acc = 0
+    #     cur_dir = 0
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             running = False
+    #         elif event.type == pygame.KEYDOWN:
+    #             if event.key == pygame.K_UP:
+    #                 cur_acc += acc
+    #             if event.key == pygame.K_DOWN:
+    #                 cur_acc -= acc
+    #             if event.key == pygame.K_RIGHT:
+    #                 cur_dir += direction
+    #             if event.key == pygame.K_LEFT:
+    #                 cur_dir -= direction
+
+            
+    #     obs, _, _ = g.step(cur_dir, cur_acc, obs)
+
+    #     im  = Image.fromarray(obs).convert("L")
+    #     im.save('outfile_{}.png'.format(counter))
+        
+
---
+++ sac.py
@@ -0,0 +1,207 @@
+import argparse
+import random
+from collections import deque
+import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions import Normal
+import game2
+
+
+# Parameters
+
+parser = argparse.ArgumentParser(description="Soft Actor-Critic",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--seed', default=1, type=int, metavar='N',
+                    help='random seed')
+parser.add_argument('--env', default='HalfCheetah-v2',
+                    help='environment id')
+parser.add_argument('--gamma', default=0.99, type=float, metavar='GAMMA',
+                    help='discount factor')
+parser.add_argument('--tau', default=0.005, type=float, metavar='TAU',
+                    help='smoothing coefficient for target network')
+parser.add_argument('--lr', default=0.0003, type=float, metavar='LR',
+                    help='learning rate')
+parser.add_argument('--replay_buffer_size', default=1000000, type=int, metavar='N',
+                    help='size of the replay buffer')
+parser.add_argument('--hidden_size', default=256, type=int, metavar='N',
+                    help='number of units in a hidden layer')
+parser.add_argument('--batch_size', default=256, type=int, metavar='N',
+                    help='batch size')
+parser.add_argument('--n_episodes', default=1000, type=int, metavar='N',
+                    help='total number of training episodes')
+parser.add_argument('--n_random_episodes', default=10, type=int, metavar='N',
+                    help='number of initial episodes for random exploration')
+args = parser.parse_args()
+
+
+# Initial Setup
+
+env = game2.game()
+obs_size, act_size = env.observation_space.shape[0], env.action_space.shape[0]
+
+# env.seed(args.seed)
+# env.action_space.seed(args.seed)
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# Networks
+
+class MLP(nn.Module):
+    def __init__(self, input_size, output_size, init_w=3e-3):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(input_size, args.hidden_size), nn.ReLU(),
+            nn.Linear(args.hidden_size, args.hidden_size), nn.ReLU(),
+            nn.Linear(args.hidden_size, args.hidden_size), nn.ReLU(),
+            nn.Linear(args.hidden_size, output_size)
+        )
+        self.net[-1].weight.data.uniform_(-init_w, init_w)
+        self.net[-1].bias.data.uniform_(-init_w, init_w)
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Critic(nn.Module):
+    """ Twin Q-networks """
+    def __init__(self):
+        super().__init__()
+        self.net1 = MLP(obs_size+act_size, 1)
+        self.net2 = MLP(obs_size+act_size, 1)
+
+    def forward(self, state, action):
+        state_action = torch.cat([state, action], 1)
+        return self.net1(state_action), self.net2(state_action)
+
+
+class Actor(nn.Module):
+    """ Gaussian Policy """
+    def __init__(self):
+        super().__init__()
+        self.net = MLP(obs_size, act_size*2)
+
+    def forward(self, state):
+        x = self.net(state)
+        mean, log_std = x[:, :act_size], x[:, act_size:]
+        log_std = torch.clamp(log_std, min=-20, max=2)
+        return mean, log_std
+
+    def sample(self, state):
+        mean, log_std = self.forward(state)
+        normal = Normal(mean, log_std.exp())
+        x = normal.rsample()
+
+        # Enforcing action bounds
+        action = torch.tanh(x)
+        #print(action)
+        log_prob = normal.log_prob(x) - torch.log(1 - action**2 + 1e-6)
+        log_prob = log_prob.sum(1, keepdim=True)
+
+        return action, log_prob
+
+    def select_action(self, state):
+        state = torch.FloatTensor(state).to(device).unsqueeze(0)
+        action, _ = self.sample(state)
+        return action[0].detach().cpu().numpy()
+
+
+critic = Critic().to(device)
+critic_optimizer = torch.optim.Adam(critic.parameters(), lr=args.lr)
+
+critic_target = Critic().to(device)
+for target_param, param in zip(critic_target.parameters(), critic.parameters()):
+    target_param.data.copy_(param.data)
+
+actor = Actor().to(device)
+actor_optimizer = torch.optim.Adam(actor.parameters(), lr=args.lr)
+
+target_entropy = -act_size
+log_alpha = torch.zeros(1, requires_grad=True, device=device)
+alpha_optimizer = torch.optim.Adam([log_alpha], lr=args.lr)
+
+
+def update_parameters(replay_buffer):
+    batch = random.sample(replay_buffer, k=args.batch_size)
+    state, action, reward, next_state, not_done = [torch.FloatTensor(t).to(device) for t in zip(*batch)]
+
+    alpha = log_alpha.exp().item()
+
+    # Update critic
+
+    with torch.no_grad():
+        next_action, next_action_log_prob = actor.sample(next_state)
+        q1_next, q2_next = critic_target(next_state, next_action)
+        q_next = torch.min(q1_next, q2_next)
+        value_next = q_next - alpha * next_action_log_prob
+        q_target = reward + not_done * args.gamma * value_next
+
+    q1, q2 = critic(state, action)
+    q1_loss = 0.5*F.mse_loss(q1, q_target)
+    q2_loss = 0.5*F.mse_loss(q2, q_target)
+    critic_loss = q1_loss + q2_loss
+
+    critic_optimizer.zero_grad()
+    critic_loss.backward()
+    critic_optimizer.step()
+
+    for target_param, param in zip(critic_target.parameters(), critic.parameters()):
+        target_param.data.copy_((1.0-args.tau)*target_param.data + args.tau*param.data)
+
+    # Update actor
+
+    action_new, action_new_log_prob = actor.sample(state)
+    q1_new, q2_new = critic(state, action_new)
+    q_new = torch.min(q1_new, q2_new)
+    actor_loss = (alpha*action_new_log_prob - q_new).mean()
+
+    actor_optimizer.zero_grad()
+    actor_loss.backward()
+    actor_optimizer.step()
+
+    # Update alpha
+
+    alpha_loss = -(log_alpha * (action_new_log_prob + target_entropy).detach()).mean()
+
+    alpha_optimizer.zero_grad()
+    alpha_loss.backward()
+    alpha_optimizer.step()
+
+
+# Training loop
+
+replay_buffer = deque(maxlen=args.replay_buffer_size)
+
+for episode in range(args.n_episodes):
+    state = env.reset()
+    episode_reward = 0
+    for episode_step in range(env._max_episode_steps):
+        if episode < args.n_random_episodes:
+            action = env.action_space.sample()
+            #print(action)
+        else:
+            action = actor.select_action(state)
+            print(action)
+
+        next_state, reward, done, _ = env.step(action)
+        episode_reward += reward
+
+        not_done = 1.0 if (episode_step+1) == env._max_episode_steps else float(not done)
+        replay_buffer.append([state, action, [reward], next_state, [not_done]])
+        state = next_state
+
+        #print(state)
+
+        if len(replay_buffer) > args.batch_size:
+            update_parameters(replay_buffer)
+
+        if done:
+            break
+
+    print("Episode {}. Reward {}".format(episode, episode_reward))
